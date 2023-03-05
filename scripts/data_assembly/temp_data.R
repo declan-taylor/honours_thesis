f021519e2c33af02a64ada5e46a9ed5e9b01b438
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-03-05 14:34:17 -0800

    Refining addT function to generate two summaries for each datatype: average temperature and daytime average temp.

--- scripts/data_assembly/temp_data.R
+++ scripts/data_assembly/temp_data.R
@@ -1,3 +1,7 @@
+# Secondary function: the main function requires knowledge of averaging 
+# temperatures based on
+
+# MAIN FUNCTION
 # This function generates a single dataframe in the global environment for all
 # of the soil or temperature data, which can then be bound to the master
 # dataframe. The function gets called in the master_dataframe.R script.
@@ -8,7 +12,12 @@ addT <- function(dataType){
                           pattern = "csv",
                           recursive = FALSE)
   
+  # NULL dataframe for all temp data
   assign(paste0(dataType, "_temp"), NULL,
+         env = .GlobalEnv)
+  
+  # NULL dataframe for daily average temp
+  assign(paste0(dataType, "_dailyAvg_T"), NULL,
          env = .GlobalEnv)
   
   for(i in file_list){
@@ -17,7 +26,8 @@ addT <- function(dataType){
       # Rename the date_time and temp columns by extracting their complete but
       # inconsistent names using the catch-all regular expressions/
       rename_all(~gsub("date.*", "datetime", .x)) %>%
-      rename_all(~gsub("temp.*", paste0(dataType, "_temp_C"), .x)) %>%
+      rename_all(~gsub("temp.*", "temp_C", .x)) %>%
+      mutate(temp_C = as.numeric(temp_C)) %>%
       # Add columns that contain site, treatment, and plot number info on each row.
       add_column("site" = str_extract(i, "[A-z]{3,5}(?=\\_)"),
                  "plot" = as.factor(str_extract(i, "[0-9]{1,2}(?=[A-z])")),
@@ -25,13 +35,38 @@ addT <- function(dataType){
       # Correct time data and generate a DOY column
       mutate(datetime = mdy_hms(datetime,
                                 tz = "Canada/Central"),
-             doy = yday(datetime)) %>%
-      select(site, plot, treatment, datetime, doy, paste0(dataType, "_temp_C"))
+             doy = yday(datetime),
+             hour = hour(datetime)) %>%
+      # Name temperature based on dataType and select appropriate columns
+      rename_all(~gsub("temp.*", paste0(dataType, "_temp_C"), .x)) %>%
+      select(site, plot, treatment, datetime, doy, hour, paste0(dataType, "_temp_C"))
     
+    # A daily average temperature will be used to plot and explore seasonal 
+    # temperature change between plots, sites, etc.
+    assign(paste0(dataType, "_dailyAvg_T"),
+           rbind(get(paste0(dataType, "_dailyAvg_T"),
+                     env = .GlobalEnv),
+           # Summarizing the temperature data.
+           temp_i %>%
+             group_by(site, plot, treatment, doy) %>%
+             summarise(dailyAvg_T = mean(get(paste0(dataType, "_temp_C")))) %>%
+             ungroup()),
+           env = .GlobalEnv)
+   
+    # A *daytime* average temperature will be used to fill in missing IRGA temperature probe data. 
     # Append the next CSV's data to the big dataframe and assign it to the global environment.
+    
+    
+    # Generate a daytime average to sub in for missing T probe data.
     assign(paste0(dataType, "_temp"),
            rbind(get(paste0(dataType, "_temp"),
-                     env = .GlobalEnv), temp_i),
+                     env = .GlobalEnv), 
+                 # Summarise the temperature data only during daytime hours.
+                 temp_i %>%
+                   group_by(site, plot, treatment, doy) %>%
+                   filter(hour >= 10 & hour <= 16) %>%
+                   summarise(daytimeT = mean(get(paste0(dataType, "_temp_C")))) %>%
+                   ungroup()),
            env = .GlobalEnv)
   }
 }
