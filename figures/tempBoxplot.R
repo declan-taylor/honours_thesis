ce96746ebfe569c322fd4bf5b29c683e76e55773
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-03-06 22:00:36 -0800

    Investigate temperature data. make boxplot.

---
+++ figures/tempBoxplot.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(here)
+
+# Generating box plots to determine difference in temperature between sites.
+# Run `temp_data.R` before using this script.
+addT("air", "all") #creates air_dailyAvg_T
+addT("soil", "all") #creates soil_dailyAvg_T
+fullTemp <- as_tibble(full_join(air_dailyAvg_T, soil_dailyAvg_T,
+                                by = c("doy", "site", "plot", "treatment"),
+                                keep = FALSE))
+
+# The boxplots
+soiltemp <- ggplot(data = fullTemp, aes(x = site,
+                                        y = soil_dailyAvg_T,
+                                        fill = treatment))+
+  geom_boxplot()+
+  geom_point(aes(colour = treatment),
+             position=position_jitterdodge(jitter.width = 0.3),
+             size = 0.7, alpha = 0.6)+
+  xlab("Site")+
+  ylab("Daily Average Soil Temperature (ºC)")+
+  scale_fill_discrete(labels=c("control (ambient)", "treatment (OTC)"))+
+  scale_colour_discrete(guide = "none")+
+  labs(fill = NULL)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+airtemp <- ggplot(data = fullTemp, aes(x = site,
+                                       y = air_dailyAvg_T,
+                                       fill = treatment))+
+  geom_boxplot()+
+  geom_point(aes(colour = treatment),
+             position=position_jitterdodge(jitter.width = 0.3),
+             size = 0.7, alpha = 0.6)+
+  xlab("Site")+
+  ylab("Daily Average Air Temperature (ºC)")+
+  scale_fill_discrete(labels=c("control (ambient)", "treatment (OTC)"),
+                      # legend turned off so that boxplots can be adjacent.
+                      guide = "none")+
+  scale_colour_discrete(guide = "none")+
+  labs(fill = NULL)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("tempBoxplot.png", plot = airtemp + soiltemp + patchwork::plot_layout(),
+       device = "png", path = here("figures"))
