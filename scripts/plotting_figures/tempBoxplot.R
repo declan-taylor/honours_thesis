c1eeb69ec881bffeaf87895c9b8bb43d314979ac
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-03-12 18:30:01 -0700

    Refined temperature figures

--- scripts/plotting_figures/tempBoxplot.R
+++ scripts/plotting_figures/tempBoxplot.R
@@ -15,14 +15,14 @@ fullTemp <- as_tibble(full_join(air_dailyAvg_T, soil_dailyAvg_T,
 soiltemp <- ggplot(data = fullTemp, aes(x = site,
                                         y = soil_dailyAvg_T,
                                         fill = treatment))+
-  geom_boxplot()+
   geom_point(aes(colour = treatment),
              position=position_jitterdodge(jitter.width = 0.3),
              size = 0.7, alpha = 0.6)+
+  geom_boxplot()+
   xlab("Site")+
   ylab("Daily Average Soil Temperature (ºC)")+
   scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
-                      labels=c("control (ambient)", "treatment (OTC)"))+
+                      labels=c("ambient", "warmed (OTC)"))+
   scale_colour_discrete(type=c("#89C5DA", "#DA5724"),
                         guide = "none")+
   labs(fill = NULL)+
@@ -33,14 +33,14 @@ soiltemp <- ggplot(data = fullTemp, aes(x = site,
 airtemp <- ggplot(data = fullTemp, aes(x = site,
                                        y = air_dailyAvg_T,
                                        fill = treatment))+
-  geom_boxplot()+
   geom_point(aes(colour = treatment),
              position=position_jitterdodge(jitter.width = 0.3),
              size = 0.7, alpha = 0.6)+
+  geom_boxplot()+
   xlab("Site")+
   ylab("Daily Average Air Temperature (ºC)")+
   scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
-                      labels=c("control (ambient)", "treatment (OTC)"),
+                      labels=c("ambient", "warmed (OTC)"),
                       # legend turned off so that boxplots can be adjacent.
                       guide = "none")+
   scale_colour_discrete(type=c("#89C5DA", "#DA5724"),
@@ -51,4 +51,84 @@ airtemp <- ggplot(data = fullTemp, aes(x = site,
         panel.grid.minor = element_blank())
 
 ggsave("tempBoxplot.png", plot = airtemp + soiltemp + patchwork::plot_layout(),
+       device = "png", path = here("figures"))
+
+
+# Trying a line figure too. With site as colour and treatment as shape.
+ggplot(data = fullTemp,
+       aes(x = doy, y = air_dailyAvg_T))+
+  geom_point(aes(colour = site,
+                 shape = treatment),
+             alpha = 0.4)+
+  geom_smooth(aes(colour = site,
+                  linetype = treatment))+
+  # Ordered DRYAS, MEAD, WILL
+  scale_colour_manual(values=c("#A5A5A5", "#DE8344", "#37518A"))+
+  theme_bw()
+
+# Line figure with just treatment.
+ggplot(data = fullTemp,
+       aes(x = doy, y = air_dailyAvg_T))+
+  geom_point(aes(colour = treatment,
+                 shape = treatment),
+             alpha = 0.3)+
+  geom_smooth(aes(colour = treatment,
+                  linetype = treatment))+
+  scale_colour_manual(values=c("#89C5DA", "#DA5724"))+
+ # scale_shape_manual(guide = "none")+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+  
+
+# Line figure with just treatment, sites are faceted.
+airtempLine <- ggplot(data = fullTemp,
+                      aes(x = doy, y = air_dailyAvg_T))+
+  geom_line(aes(colour = treatment,
+                 group = plot),
+             alpha = 0.1)+
+  geom_smooth(aes(colour = treatment,
+                  fill = treatment,
+                  linetype = treatment),
+              size = 1.1,
+              alpha = 0.5)+
+  facet_wrap(~ site, scales = "free")+ 
+  scale_colour_manual(values=c("#89C5DA", "#DA5724"),
+                      labels=c("ambient", "warmed (OTC)"))+
+  scale_linetype(labels=c("ambient", "warmed (OTC)"))+
+  scale_fill_manual(values=c("#89C5DA", "#DA5724"),
+                    guide = "none")+
+  xlab(NULL)+
+  ylab("Daily Average Air Temperature (ºC)")+
+  labs(colour = NULL,
+       linetype = NULL)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+soiltempLine <- ggplot(data = fullTemp,
+                      aes(x = doy, y = soil_dailyAvg_T))+
+  geom_line(aes(colour = treatment,
+                group = plot),
+            alpha = 0.15)+
+  geom_smooth(aes(colour = treatment,
+                  fill = treatment,
+                  linetype = treatment),
+              size = 1.1,
+              alpha = 0.5)+
+  facet_wrap(~ site, scales = "free")+ 
+  scale_colour_manual(values=c("#89C5DA", "#DA5724"),
+                      labels=c("ambient", "warmed (OTC)"))+
+  scale_linetype(labels=c("ambient", "warmed (OTC)"))+
+  scale_fill_manual(values=c("#89C5DA", "#DA5724"),
+                    guide = "none")+
+  xlab("Day of Year")+
+  ylab("Daily Average  Soil Temperature (ºC)")+
+  labs(colour = NULL,
+       linetype = NULL)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("tempLine.png", plot = airtempLine + soiltempLine + patchwork::plot_layout(ncol = 1),
        device = "png", path = here("figures"))
