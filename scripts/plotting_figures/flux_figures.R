7c1095d549d70fab3e9f48f6537118cb2a154ff8
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-03-12 19:34:12 -0700

    Figure making bonanza!

--- scripts/plotting_figures/flux_figures.R
+++ scripts/plotting_figures/flux_figures.R
@@ -1,14 +1,65 @@
+# Load data and libraries
+source(here("scripts/assembly/master_dataframe.R"))
 library(ggplot2)
 
-NEE.boxplot <- ggplot(data = NEE) +
+# NEE, GEP, ER fluxes represented by C/T boxplots
+NEE.boxplot <- ggplot(data = NEE)+
   geom_boxplot(aes(x = treatment,
                    y = NEE_umol_s_m2,
                    fill = treatment))+
-  facet_wrap(~ site, scales = "free")+ 
+  geom_hline(yintercept = 0, linetype = 2, colour = "darkgrey", alpha = 1)+
+  facet_wrap(~ site)+ 
+  scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
+                      guide = "none")+
+  scale_x_discrete(labels=c("ambient", "warmed (OTC)"))+
+  labs(fill = NULL,
+       x = "Treatment",
+       y = bquote('Net Ecosystem Exchange (NEE; ' *mu~'mol' ~CO[2]~ m^-2~s^-1*')'))+
   theme_bw()+
-  scale_fill_manual(values=c( "#89C5DA", "#DA5724"))+
-  labs(fill="Treatment")+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
-  
+GEP.boxplot <- ggplot(data = GEP) +
+  geom_boxplot(aes(x = treatment,
+                   y = GEP_umol_s_m2,
+                   fill = treatment))+
+  geom_hline(yintercept = 0, linetype = 2, colour = "darkgrey", alpha = 1)+
+  facet_wrap(~ site)+ 
+  scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
+                      guide = "none")+
+  scale_x_discrete(labels=c("ambient", "warmed (OTC)"))+
+  labs(fill = NULL,
+            x = "Treatment",
+            y = bquote('Gross Ecosystem Productivity (GEP; ' *mu~'mol' ~CO[2]~ m^-2~s^-1*')'))+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ER.boxplot <- ggplot(data = ER) +
+  geom_boxplot(aes(x = treatment,
+                   y = ER_umol_s_m2,
+                   fill = treatment))+
+  geom_hline(yintercept = 0, linetype = 2, colour = "darkgrey", alpha = 1)+
+  facet_wrap(~ site)+ 
+  scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
+                      guide = "none")+
+  scale_x_discrete(labels=c("ambient", "warmed (OTC)"))+
+  labs(fill = NULL,
+       x = "Treatment",
+       y = bquote('Ecosystem Respiration (ER; ' *mu~'mol' ~CO[2]~ m^-2~s^-1*')'))+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("NEE_boxplot.png", plot = NEE.boxplot,
+       device = "png", path = here("figures"))
+ggsave("GEP_boxplot.png", plot = GEP.boxplot,
+       device = "png", path = here("figures"))
+ggsave("ER_boxplot.png", plot = ER.boxplot,
+       device = "png", path = here("figures"))
+
+ggsave("flux_boxplots.png", plot = GEP.boxplot+labs(x = NULL) + 
+                                    ER.boxplot+labs(x = NULL) + 
+                                    NEE.boxplot + patchwork::plot_layout(ncol = 1),
+       height = 4000, width = 3000, units = "px",
+       device = "png", path = here("figures"))
--- scripts/plotting_figures/geiBoxplot.R
+++ scripts/plotting_figures/geiBoxplot.R
@@ -5,11 +5,18 @@ source(here("scripts/data_assembly/GEI.R"))
 generate_GEI(here("data/greenness/cropped_images"))
 
 GEIboxplot <- ggplot(data = GEI,
-                     aes(x = site, y = GEI))+
+                     aes(x = site, y = GEI))+ 
   geom_boxplot(aes(fill = treatment))+
-  scale_fill_manual(values=c( "#89C5DA", "#DA5724"))+
+  xlab("Site")+
+  ylab("Greeness Excess Index (GEI)")+
+  scale_fill_discrete(type=c("#89C5DA", "#DA5724"),
+                      labels=c("ambient", "warmed (OTC)"))+
+  labs(fill = NULL)+
   theme_bw()+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
-ggsave()
+  # IS GEI SIGNIFICANTLY DIFFERENT BETWEEN SITES?
+  
+ggsave("GEIboxplot.png", plot = GEIboxplot,
+       device = "png", path = here("figures"))
